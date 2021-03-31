06867c141af52276cb173020e60d78193af27cd4
kitoko2 <dogbo804@gmail.com>
2021-03-31 13:53:52 +0000

    sauvegarde projet

---
+++ projet.dart
@@ -0,0 +1,92 @@
+main() {
+  // Robot robo = new Robot(8, 4, 2);
+
+  // print(robo.direction);
+  // robo.instruction("GAAADDDAGGGG");
+  // print("position x : ${robo.x}");
+  // print("position y : ${robo.y}");
+  // print("orientation : ${robo.direction}");
+
+  print("---------------------------------------------");
+  //Robot(x,y,<la face(0,1,2,3 pour Nord,Est,Sud,Ouest)>)
+  Robot robo1 = new Robot(0, 0, 0);
+  Robot robo2 = new Robot(0, -1, 0);
+
+  //CHANGEMENT DE LA POSITION VERS LA FACE EST
+  // print(robo2.direction);
+  robo1.instruction("D");
+  robo2.instruction("D");
+  // print(robo2.direction);
+  //CHANGEMENT DE LA POSITION VERS LA FACE OUEST
+  robo1.instruction("GG");
+  robo2.instruction("GG");
+  // tourner 2 fois a gauche permet de changer la face a Nord, puis Ouest
+  // print(robo2.direction);
+
+  //SERIE D'INSTRUCTION
+  //1
+  print("----------INSTRUCTION 1-------------------");
+  Robot r = new Robot(7, 3, 0);
+  r.instruction("DAAGAG");
+  print("la nouvelle position(x,y) de r est: (${r.x},${r.y})");
+  print("sa nouvelle direction est le(l') ${r.direction}");
+
+  //2
+  print("\n----------INSTRUCTION 2-------------------");
+  Robot r2 = new Robot(2, -7, 3);
+  r2.instruction("DDAAAAAGA");
+  print("la nouvelle position(x,y) de r2 est: (${r2.x},${r2.y})");
+  print("sa nouvelle direction est le(l') ${r2.direction}");
+
+  //3
+  print("\n----------INSTRUCTION 3-------------------");
+  Robot r3 = new Robot(8, 4, 2);
+  r3.instruction("GAAADDDAGGGG");
+  print("la nouvelle position(x,y) de r3 est: (${r3.x},${r3.y})");
+  print("sa nouvelle direction est le(l') ${r3.direction}");
+}
+
+class Robot {
+  int x = 0;
+  int y = 0;
+  int face = 0; //0 pour nord,1 pour ouest 2 pour sud 3 pour Est
+  String? direction;
+  List changeface = ["Nord", "Est", "Sud", "Ouest"];
+
+  Robot(int x, int y, int face) {
+    this.x = x;
+    this.y = y;
+    this.face = face;
+    this.direction = changeface[face];
+  }
+  instruction(String instruction) {
+    for (int i = 0; i < instruction.length; i++) {
+      if (instruction[i] == "D") {
+        face++;
+        if (face > 3) {
+          face = 0;
+        }
+        this.direction = changeface[face];
+      }
+      if (instruction[i] == "G") {
+        face--;
+        if (face < 0) {
+          face = 3;
+        }
+        this.direction = changeface[face];
+      }
+      if (instruction[i] == "A" && this.direction == "Nord") {
+        this.y = this.y + 1;
+      }
+      if (instruction[i] == "A" && this.direction == "Sud") {
+        this.y = this.y - 1;
+      }
+      if (instruction[i] == "A" && this.direction == "Ouest") {
+        this.x = this.x - 1;
+      }
+      if (instruction[i] == "A" && this.direction == "Est") {
+        this.x = this.x + 1;
+      }
+    }
+  }
+}
